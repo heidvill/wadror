2bea230bb2020c551778a92e8f53946c7e8c6b9c
Heidi Villikka <heidi.villikka@helsinki.fi>
2017-02-17 15:17:16 +0200

    session toimii

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return nil if session[:user_id].nil?
-    #User.find(session[:user_id])
-    nil
+    User.find(session[:user_id])
   end
 
   def ensure_that_signed_in
